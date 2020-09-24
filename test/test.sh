c6de6f2fe26d88f805da1958e57552b1aad68053
DarwinNE <davbucci@gmail.com>
2020-09-24 22:30:53 +0200

    Added automatic tests that chek if Dawn Patrol and Iunnuh can be successfully converted, built and played to the end.

---
+++ test/test.sh
@@ -0,0 +1,10 @@
+printf "AWS test suite\n"
+printf "Davide Bucci 2020\n\n"
+
+cd test_dp
+./test_dp.sh
+cd ..
+
+cd test_iunnuh
+./test_iunnuh.sh
+cd ..
---
+++ test/test_dp/test_dp.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+red_code="\033[1;31m"
+green_code="\033[32m"
+normal_code="\033[0m"
+
+printf "Compiling Dawn Patrol with -d -n options... "
+cd ../../DawnPatrol
+
+if make modern >compile_results.txt 2>compile_errors.txt
+then
+    printf $green_code"PASS\n"$normal_code
+else
+    printf $red_code"FAIL\n"$normal_code
+    exit
+fi
+
+printf "Running Dawn Patrol with walktrough........ "
+
+./patrol <solution.txt >test_results.txt
+
+# Search for a sentence present in the message shown when the game is complete
+
+if grep -q "mates in time" test_results.txt
+then
+    printf $green_code"PASS\n"$normal_code
+else
+    printf $red_code"FAIL\n"$normal_code
+    exit
+fi
+
+
+printf "Compiling Dawn Patrol with -d -c -m -n -s.. "
+
+if make modern_alt >compile_results.txt 2>compile_errors.txt
+then
+    printf $green_code"PASS\n"$normal_code
+else
+    printf $red_code"FAIL\n"$normal_code
+    exit
+fi
+
+printf "Running Dawn Patrol with walktrough........ "
+
+./patrol_a <solution.txt >test_results.txt
+
+# Search for a sentence present in the message shown when the game is complete
+
+if grep -q "mates in time" test_results.txt
+then
+    printf $green_code"PASS\n"$normal_code
+else
+    printf $red_code"FAIL\n"$normal_code
+    exit
+fi
