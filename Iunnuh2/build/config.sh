17b5b167fcec42cde1ff944529f5bca5f68bd698
DarwinNE <davbucci@gmail.com>
2026-01-10 21:03:49 +0100

    Add config.sh and readme.txt files, forgotten in the previous commits.

---
+++ Iunnuh2/build/config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Specify here the directory where the distribution files will be copied:
+ditdir="/Users/davidebucci/Documents/Synchronized/mysite/varie/iunnuh2"
+
+# Path to the c1541 tool (handle .d64 disks):
+c1541command="/Applications/Emulators/VICE2.4/tools/c1541"
+
+# Path to the m20 disk command:
+m20command="m20"
+
+# Path to the xdftool (for Amiga adf disks)
+xdftoolcommand="/Users/davidebucci/Documents/Synchronized/Vintage/Computing/Commodore/Amiga/amitools/bin/xdftool"
+
+# Directory where the CPC mastering tools are present:
+pp="/Users/davidebucci/Documents/Synchronized/Vintage/Computing/Amstrad/CPC/cpc-mastering"
+
+# Python script of the mkcas utility:
+mkcas="/Users/davidebucci/Documents/Synchronized/Vintage/Computing/Amstrad/mkcas/mkcas.py"
+
+# cas2wav (part of castools for MSX computers).
+cas2wav="/Users/davidebucci/Documents/Synchronized/Vintage/Computing/MSX/castools/cas2wav"
+
+# Full filename of Apple Commander (it requires Java)
+acjarfile="/Users/davidebucci/Documents/Synchronized/Vintage/Computing/Apple/AppleII/AppleCommander-ac-13.0.jar"
+
+# Full filename of Atari disk tools
+ataritools="/Users/davidebucci/Documents/Synchronized/Vintage/Computing/Atari/atari-tools/atr"
